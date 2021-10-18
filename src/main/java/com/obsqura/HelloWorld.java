8172be6a0c81ef84bd03b7bde036c6ddd7fea13a
user <navyanaveenam@gmail.com>
2021-10-19 00:09:06 +0530

    Second Commit

--- src/main/java/com/obsqura/HelloWorld.java
+++ src/main/java/com/obsqura/HelloWorld.java
@@ -2,6 +2,10 @@ package com.obsqura;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HEllo World");
+        System.out.println("Hello World");
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+
     }
 }
